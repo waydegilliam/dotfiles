42e58e7841f1f1cd52daad8c25d8c014c8ecb6f4
waydegilliam <waydegilliam@gmail.com>
2026-01-02 19:48:05 -0800

    fish: improve git worktree list

--- fish/.config/fish/functions/gwl.fish
+++ fish/.config/fish/functions/gwl.fish
@@ -1,3 +1,17 @@
 function gwl --description "List all git worktrees"
-  git worktree list
+  set -l current (git rev-parse --show-toplevel)
+  set -l worktrees (git worktree list)
+
+  for line in $worktrees
+    set -l path (string split -f1 ' ' -- $line)
+    set -l folder (basename $path)
+    set -l branch (string match -rg '\[(.+)\]' -- $line)
+
+    set -l prefix "  "
+    if test "$path" = "$current"
+      set prefix "* "
+    end
+
+    printf "%s%-30s %s\n" $prefix $folder $branch
+  end
 end
